221172f3a19a8791833fe3104e232ea4d4b7dded
Luis <ld_ramirez27@outlook.com>
2019-11-17 22:08:21 -0500

    Agregué actualizar,eliminar e insertar para alumno y profesor (Servicios, DAO's y DBController)

---
+++ SinapxonSoftController/src/pe/edu/pucp/sinapxon/dao/PaisDAO.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pe.edu.pucp.sinapxon.dao;
+
+import java.util.ArrayList;
+import pe.edu.pucp.sinapxon.model.Pais;
+
+/**
+ *
+ * @author ld_ra
+ */
+public interface PaisDAO {
+    public ArrayList<Pais> listarPais();
+}
--- SinapxonSoftController/src/pe/edu/pucp/sinapxon/mysql/AlumnoMySQL.java
+++ SinapxonSoftController/src/pe/edu/pucp/sinapxon/mysql/AlumnoMySQL.java
@@ -8,11 +8,14 @@ package pe.edu.pucp.sinapxon.mysql;
 import java.sql.CallableStatement;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
 import pe.edu.pucp.sinapxon.config.DBManager;
 import pe.edu.pucp.sinapxon.dao.AlumnoDAO;
 import pe.edu.pucp.sinapxon.model.Alumno;
+import pe.edu.pucp.sinapxon.model.Pais;
 
 /**
  *
@@ -25,7 +28,8 @@ public class AlumnoMySQL implements AlumnoDAO{
     CallableStatement cs;
     
     @Override
-    public void insertarAlumno(Alumno alumno) {
+    public int insertarAlumno(Alumno alumno) {
+        int resultado = 0;
         try{
             con = DriverManager.getConnection(DBManager.url, DBManager.user, DBManager.password);
             cs = con.prepareCall("{call INSERTAR_ALUMNO(?,?,?,?,?,?,?,?,?,?,?)}");
@@ -41,11 +45,92 @@ public class AlumnoMySQL implements AlumnoDAO{
             cs.setString("_CORREO", alumno.getCorreo());
             cs.setString("_TELEFONO", alumno.getTelefono());
             cs.executeUpdate();
+            resultado = 1;
         }catch(SQLException ex){
             System.out.println(ex.getMessage());
         }finally{
             try{con.close();}catch(SQLException ex){System.out.println(ex.getMessage());}
         }
+        return resultado;
+    }
+
+    @Override
+    public int actualizarAlumno(Alumno alumno) {
+        int resultado = 0;
+        try{
+            con = DriverManager.getConnection(DBManager.url, DBManager.user, DBManager.password);
+            con.setAutoCommit(false);
+            cs = con.prepareCall("{call ACTUALIZAR_ALUMNO(?,?,?,?,?,?,?,?,?,?,?,?)}");
+            cs.setString("_ID_ALUMNO", alumno.getCodigo());
+            cs.setInt("_FID_PAIS", alumno.getPais().getId_pais());
+            cs.setString("_NICKNAME", alumno.getNickname());
+            cs.setString("_PASSWORD",alumno.getPassword());
+            cs.setString("_NOMBRE",alumno.getNombre());
+            cs.setString("_AP_PATERNO",alumno.getApellidoPaterno());
+            cs.setString("_AP_MATERNO",alumno.getApellidoMaterno());
+            cs.setDate("_FECHA_NACIMIENTO", new java.sql.Date(alumno.getFecha().getTime()));
+            cs.setString("_DNI",alumno.getDni());
+            cs.setString("_CORREO",alumno.getCorreo());
+            cs.setString("_TELEFONO", alumno.getTelefono());
+            cs.setInt("_ESTADO",alumno.getEstado());
+            cs.executeUpdate();
+            resultado = 1;
+        }catch(SQLException ex){
+            System.out.println(ex.getMessage());
+            try{con.rollback();}catch(SQLException exe){System.out.println(exe.getMessage());}
+        }finally{
+            try{con.setAutoCommit(true);cs.close();con.close();}catch(SQLException ex){System.out.println(ex.getMessage());}
+        }
+        return resultado;
+    }
+
+    @Override
+    public int eliminarAlumno(String idAlumno) {
+        int resultado = 0;
+        try{
+            con = DriverManager.getConnection(DBManager.url, DBManager.user, DBManager.password);
+            cs = con.prepareCall("{call ELIMINAR_ALUMNO(?)}");
+            cs.setString("_ID_ALUMNO", idAlumno);
+            resultado = cs.executeUpdate();
+        }catch(SQLException ex){
+            System.out.println(ex.getMessage());
+        }
+        return resultado;
+    }
+
+    @Override
+    public ArrayList<Alumno> listarAlumnos(String nombre) {
+        ArrayList<Alumno> alumnos = new ArrayList<>();
+        try{
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            con = DriverManager.getConnection(DBManager.url, DBManager.user, DBManager.password);
+            cs = con.prepareCall("{call LISTAR_ALUMNOS(?)}");
+            cs.setString("_NOMBRE_ALUMNO", nombre);
+            ResultSet rs = cs.executeQuery();
+            while(rs.next()){
+                Alumno alum = new Alumno();
+                alum.setApellidoMaterno(rs.getString("AP_MATERNO"));
+                alum.setApellidoPaterno(rs.getString("AP_PATERNO"));
+                alum.setCodigo(rs.getString("CODIGO"));
+                alum.setCorreo(rs.getString("CORREO"));
+                alum.setDni(rs.getString("DNI"));
+                alum.setFecha(new java.sql.Date(rs.getDate("FECHA_NACIMIENTO").getTime()));
+                alum.setNickname(rs.getString("NICKNAME"));
+                alum.setNombre(rs.getString("NOMBRE"));
+                Pais pais = new Pais();
+                pais.setId_pais(rs.getInt("FID_PAIS"));
+                pais.setNombre(rs.getString("PAIS"));
+                alum.setPais(pais);
+                alum.setPassword(rs.getString("PASSWORD"));
+                alum.setTelefono(rs.getString("TELEFONO"));
+                alumnos.add(alum);
+            }
+        }catch(ClassNotFoundException | SQLException ex){
+            System.out.println(ex.getMessage());
+        }finally{
+            try{con.close();}catch(SQLException ex){System.out.println(ex.getMessage());}
+        }
+        return alumnos;
     }
     
     
